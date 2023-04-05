b2a9c97311e3cdec4d11d2e84ca6173caea5c55e
Олег Потуданский <olegovych@bk.ru>
2023-04-05 21:16:02 +0300

    init

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import argparse
+# from difference_finder import gen_diff
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
