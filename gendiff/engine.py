c648839f8e3b7a28c41a2ad01b4a6b23cc606517
Олег Потуданский <olegovych@bk.ru>
2023-04-20 14:26:20 +0300

    update engine.py separate generation and output

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,23 +1,33 @@
 import json
+import itertools
+
+
+def make_line(prefix, key, value):
+    if isinstance(value, bool):
+        value = str(value).lower()
+    return f'  {prefix} {key}: {value}'
 
 
 def generate_diff(file_path1, file_path2):
     file_data1 = json.load(open(file_path1))
     file_data2 = json.load(open(file_path2))
     unique_keys = sorted(set(file_data1) | set(file_data2))
-    difference = ['{']
+    difference = []
     for key in unique_keys:
         if key in file_data1:
+            val1 = file_data1[key]
             if key in file_data2:
-                val1, val2 = file_data1[key], file_data2[key]
+                val2 = file_data2[key]
                 if val1 == val2:
-                    difference.append(f'    {key}: {val1}')
+                    difference.append((' ', key, val1))
                 else:
-                    difference.append(f'  - {key}: {val1}')
-                    difference.append(f'  + {key}: {val2}')
+                    difference.append(('-', key, val1))
+                    difference.append(('+', key, val2))
             else:
-                difference.append(f'  - {key}: {file_data1[key]}')
+                difference.append(('-', key, val1))
         else:
-            difference.append(f'  + {key}: {file_data2[key]}')
-    difference.append('}')
-    return '\n'.join(difference)
+            val2 = file_data2[key]
+            difference.append(('+', key, val2))
+    lines = itertools.starmap(make_line, difference)
+    result = itertools.chain("{", lines, "}")
+    return '\n'.join(result)
