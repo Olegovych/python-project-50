a563397e9349df9a2bf67e754baba64f6d47cfd0
Олег Потуданский <olegovych@bk.ru>
2023-04-22 23:33:42 +0300

    add yaml plain test

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,10 +1,14 @@
+import pytest
 from gendiff import generate_diff
 
 
-# @pytest.mark.parametrize()
-def test_generate_diff():
-    file_path1 = 'tests/fixtures/file1.json'
-    file_path2 = 'tests/fixtures/file2.json'
+@pytest.mark.parametrize(
+    'file_path1,file_path2', [
+        ('tests/fixtures/file1.json', 'tests/fixtures/file2.json'),
+        ('tests/fixtures/plain1.yml', 'tests/fixtures/p;ain2.yaml')
+    ]
+)
+def test_generate_diff_plain(file_path1, file_path2):
     difference = generate_diff(file_path1, file_path2)
     with open('tests/fixtures/plain_diff.txt') as diff_file:
         expected = diff_file.read()
