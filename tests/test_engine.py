dc030fde1a727c76ffe2cc66d8df7367391fcdb3
Олег Потуданский <olegovych@bk.ru>
2023-04-24 11:27:37 +0300

    typo

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -5,7 +5,7 @@ from gendiff import generate_diff
 @pytest.mark.parametrize(
     'file_path1,file_path2', [
         ('tests/fixtures/file1.json', 'tests/fixtures/file2.json'),
-        ('tests/fixtures/plain1.yml', 'tests/fixtures/p;ain2.yaml')
+        ('tests/fixtures/plain1.yml', 'tests/fixtures/plain2.yaml')
     ]
 )
 def test_generate_diff_plain(file_path1, file_path2):
