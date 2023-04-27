733ca317544fe006436ab7f614a05ca5f37ab975
Олег Потуданский <olegovych@bk.ru>
2023-04-27 21:49:49 +0300

    add tests for nested structure

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -13,3 +13,16 @@ def test_generate_diff_plain(file_path1, file_path2):
     with open('tests/fixtures/plain_diff.txt') as diff_file:
         expected = diff_file.read()
     assert difference == expected
+
+
+@pytest.mark.parametrize(
+    'file_path1,file_path2', [
+        ('tests/fixtures/nested1.json', 'tests/fixtures/nested2.json'),
+        ('tests/fixtures/nested1.yml', 'tests/fixtures/nested2.yaml')
+    ]
+)
+def test_generate_diff_nested(file_path1, file_path2):
+    difference = generate_diff(file_path1, file_path2)
+    with open('tests/fixtures/nested_diff.txt') as diff_file:
+        expected = diff_file.read()
+    assert difference == expected
