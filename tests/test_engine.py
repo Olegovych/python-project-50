4c6a113ebc39527863313f24e7c1aeb0f4d27ea8
Олег Потуданский <olegovych@bk.ru>
2023-04-27 22:13:40 +0300

    rename some fixtures

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -5,12 +5,12 @@ from gendiff import generate_diff
 @pytest.mark.parametrize(
     'file_path1,file_path2', [
         ('tests/fixtures/file1.json', 'tests/fixtures/file2.json'),
-        ('tests/fixtures/plain1.yml', 'tests/fixtures/plain2.yaml')
+        ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yaml')
     ]
 )
 def test_generate_diff_plain(file_path1, file_path2):
     difference = generate_diff(file_path1, file_path2)
-    with open('tests/fixtures/plain_diff.txt') as diff_file:
+    with open('tests/fixtures/flat_diff.txt') as diff_file:
         expected = diff_file.read()
     assert difference == expected
 
