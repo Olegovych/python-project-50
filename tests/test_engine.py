8eab4e1f5b947abc91415666f8e2bbdbaa25fedd
Олег Потуданский <olegovych@bk.ru>
2023-04-29 01:49:15 +0300

    add tests for plain format

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -8,7 +8,7 @@ from gendiff import generate_diff
         ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yaml')
     ]
 )
-def test_generate_diff_plain(file_path1, file_path2):
+def test_generate_diff_flat(file_path1, file_path2):
     difference = generate_diff(file_path1, file_path2)
     with open('tests/fixtures/flat_diff.txt') as diff_file:
         expected = diff_file.read()
@@ -24,5 +24,18 @@ def test_generate_diff_plain(file_path1, file_path2):
 def test_generate_diff_nested(file_path1, file_path2):
     difference = generate_diff(file_path1, file_path2)
     with open('tests/fixtures/nested_diff.txt') as diff_file:
+        expected = diff_file.read()
+    assert difference == expected
+
+
+@pytest.mark.parametrize(
+    'file_path1,file_path2', [
+        ('tests/fixtures/nested1.json', 'tests/fixtures/nested2.json'),
+        ('tests/fixtures/nested1.yml', 'tests/fixtures/nested2.yaml')
+    ]
+)
+def test_generate_diff_plain(file_path1, file_path2):
+    difference = generate_diff(file_path1, file_path2, format_name='plain')
+    with open('tests/fixtures/plain_diff.txt') as diff_file:
         expected = diff_file.read()
     assert difference == expected
