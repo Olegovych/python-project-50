57179b5a2f96908d9c8f590b17a5ac1d5c34326a
Олег Потуданский <olegovych@bk.ru>
2023-05-22 21:38:53 +0300

    separate parse and read in file_parcer

--- gendiff/file_parcer.py
+++ gendiff/file_parcer.py
@@ -2,15 +2,15 @@ import json
 import yaml
 
 
-def read_file(path):
-    with open(path, 'r') as file:
-        return file.read()
+def parse(data, format_name):
+    if format_name == 'json':
+        return json.loads(data)
+    elif format_name == 'yml' or format_name == 'yaml':
+        return yaml.safe_load(data)
 
 
-def get_data(path):
-    str_data = read_file(path)
-    ext = path.rpartition('.')[2]
-    if ext == 'json':
-        return json.loads(str_data)
-    elif ext == 'yml' or ext == 'yaml':
-        return yaml.safe_load(str_data)
+def get_data(file_path):
+    with open(file_path, 'r') as file:
+        str_data = file.read()
+    format_name = file_path.rpartition('.')[-1]
+    return parse(str_data, format_name)
