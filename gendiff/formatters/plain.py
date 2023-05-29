6539a7a8a5352a95099916ff05d7e0324eb788a9
Олег Потуданский <olegovych@bk.ru>
2023-05-29 21:01:52 +0300

    refactor plain new structure

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,45 +1,44 @@
+from gendiff.formatters.stylish import format_to_js_style
+
+
 def format_to_plain(value):
     match value:
-        case bool(val):
-            return str(val).lower()
-        case None:
-            return 'null'
-        case str(val):
-            return f"'{val}'"
         case dict(val):
             return '[complex value]'
+        case str(val):
+            return f"'{val}'"
         case _:
-            return str(value)
+            return format_to_js_style(value)
 
 
-def make_lines(branch, path=''):
+def iter_children(children, path=''):
     lines = []
-    for key, description in branch.items():
-        key_path = f'{path}.{str(key)}' if path else str(key)
-        description = list(description.items())
-        key_status, value = description[0]
-        if key_status != 'nested':
-            value = format_to_plain(value)
+    children_iterator = iter(children)
+    for child in children_iterator:
+        key = child['key']
+        property = f'{path}.{key}' if path else key
 
-        match key_status:
-            case 'deleted':
-                string_completion = "removed"
-            case 'added':
-                string_completion = f"added with value: {value}"
-            case 'changed_from':
-                _, new_value = description[1]
-                new_value = format_to_plain(new_value)
-                string_completion = f"updated. From {value} to {new_value}"
-            case 'nested':
-                nested_lines = make_lines(value, key_path)
-                lines.extend(nested_lines)
-                continue
-            case _:
-                continue
+        type_ = child['type']
+        if type_ == 'nested':
+            nested_lines = iter_children(child['children'], property)
+            lines.extend(nested_lines)
+            continue
 
-        lines.append(f"Property '{key_path}' was {string_completion}")
+        value = format_to_plain(child['value'])
+        if type_ == 'deleted':
+            action = "removed"
+        elif type_ == 'added':
+            action = f"added with value: {value}"
+        elif type_ == 'changed_from':
+            child = next(children_iterator)
+            new_value = format_to_plain(child['value'])
+            action = f"updated. From {value} to {new_value}"
+        else:
+            continue
+
+        lines.append(f"Property '{property}' was {action}")
     return lines
 
 
 def make_plain(diff):
-    return '\n'.join(make_lines(diff))
+    return '\n'.join(iter_children(diff['children']))
