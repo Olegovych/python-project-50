df60d7b65cf46fe8c7dcb5e39641399feab6733b
Олег Потуданский <olegovych@bk.ru>
2023-05-29 21:00:11 +0300

    refactor stylish new structure

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,5 +1,3 @@
-import itertools
-
 REPLACER = ' '
 SPACES_COUNT = 4
 OFFSET = 2
@@ -9,42 +7,57 @@ SIGNS = {
     'unchanged': '  ',
     'nested': '  ',
     'changed_from': '- ',
-    'changed_to': '+ '
+    'changed_to': '+ ',
+    'blanc': '  '
 }
 
 
-def format_to_str(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    elif value is None:
-        return 'null'
-    else:
-        return str(value)
+def format_to_js_style(value):
+    match value:
+        case bool(val):
+            return str(val).lower()
+        case None:
+            return 'null'
+        case _:
+            return value
 
 
-def make_stylish(branch, depth=1, is_child=True):
-    def make_line():
-        processed_value = make_stylish(value, depth + 1, is_child)
-        lines.append(f'{indent}{sign}{key}: {processed_value}')
+def make_indent(depth, sign):
+    size = SPACES_COUNT * depth - OFFSET
+    blanc = REPLACER * size
+    return f'{blanc}{sign}' if depth else ''
 
-    if not isinstance(branch, dict):
-        return format_to_str(branch)
 
-    indent_size = SPACES_COUNT * depth - OFFSET
-    indent = REPLACER * indent_size
-    lines = []
-
-    for key, description in branch.items():
-        if not is_child:
-            sign = REPLACER * OFFSET
-            value = description
-            make_line()
+def dict_processing(dict_, depth):
+    lines = ['{']
+    indent = make_indent(depth, sign=SIGNS['blanc'])
+    for key, val in dict_.items():
+        if isinstance(val, dict):
+            value = dict_processing(val, depth + 1)
         else:
-            for key_status, value in description.items():
-                sign = SIGNS[key_status]
-                is_child = True if key_status == 'nested' else False
-                make_line()
-                is_child = True
-    last_indent = REPLACER * (SPACES_COUNT * (depth - 1))
-    result = itertools.chain("{", lines, [last_indent + "}"])
-    return '\n'.join(result)
+            value = format_to_js_style(val)
+        lines.append(f'{indent}{key}: {value}')
+    indent = make_indent(depth - 1, sign=SIGNS['blanc'])
+    lines.append(indent + '}')
+    return '\n'.join(lines)
+
+
+def children_processing(children, depth):
+    lines = ['{']
+    for child in children:
+        indent = make_indent(depth, sign=SIGNS[child['type']])
+        property = child['key']
+        if child['type'] == 'nested':
+            value = children_processing(child['children'], depth + 1)
+        elif isinstance(child['value'], dict):
+            value = dict_processing(child['value'], depth + 1)
+        else:
+            value = format_to_js_style(child['value'])
+        lines.append(f'{indent}{property}: {value}')
+    indent = make_indent(depth - 1, sign=SIGNS['blanc'])
+    lines.append(indent + '}')
+    return '\n'.join(lines)
+
+
+def make_stylish(diff):
+    return children_processing(diff['children'], depth=1)
