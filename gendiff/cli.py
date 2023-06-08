d44cd944e4541787f92c8544032a6cc787ee2a25
Олег Потуданский <olegovych@bk.ru>
2023-06-08 10:16:02 +0300

    refactor some naming

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -3,14 +3,16 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser(
+        prog='gendiff',
         description='Compares two configuration files and shows a difference.'
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', default='stylish',
-                        help='set format of output')
+    parser.add_argument(
+        '-f', '--format',
+        default='stylish',
+        help='set format of output'
+    )
     args = parser.parse_args()
-    file_path1 = args.first_file
-    file_path2 = args.second_file
-    formatter = args.format
-    return file_path1, file_path2, formatter
+
+    return args.first_file, args.second_file, args.format
--- gendiff/file_parcer.py
+++ gendiff/file_parcer.py
@@ -2,15 +2,15 @@ import json
 import yaml
 
 
-def parse(data, format_name):
-    if format_name == 'json':
+def parse(data, file_format):
+    if file_format == 'json':
         return json.loads(data)
-    elif format_name == 'yml' or format_name == 'yaml':
+    elif file_format in ('yml', 'yaml'):
         return yaml.safe_load(data)
 
 
 def get_data(file_path):
     with open(file_path, 'r') as file:
         data = file.read()
-    format_name = file_path.rpartition('.')[-1]
-    return parse(data, format_name)
+    file_format = file_path.rpartition('.')[-1]
+    return parse(data, file_format)
