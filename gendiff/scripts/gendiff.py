28dbbea9ddd4fb92033722b9cbc53e5f3521e084
Олег Потуданский <olegovych@bk.ru>
2023-06-14 11:29:36 +0300

    fix script return to print

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,7 @@ from gendiff.differ import generate_diff
 def main():
     file_path1, file_path2, formatter = get_args()
     difference = generate_diff(file_path1, file_path2, formatter)
-    return difference
+    print(difference)
 
 
 if __name__ == '__main__':
